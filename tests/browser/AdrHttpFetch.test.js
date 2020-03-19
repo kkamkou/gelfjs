3e3a1cce0c7caace47fa637e622097c260e440ce
Kanstantsin Kamkou <me@2ka.by>
2020-03-19 14:30:23 +0100

    package.json correction

--- tests/browser/AdrHttpFetch.test.js
+++ tests/browser/AdrHttpFetch.test.js
@@ -5,5 +5,5 @@ import GelfJs from "../../gelfjs/lib/GelfJs";
 xtest('asd', async () => {
   const gelfjs = new GelfJs.Smart(new AdrHttpFetch.Smart("http://xx/gelf")),
     extra = {tom: 'cat', dogs: {spike: 'bulldog', tyke: 'son of spike'}};
-  await gelfjs.warn('a new message', extra);
+  //await gelfjs.warn('a new message', extra);
 });
