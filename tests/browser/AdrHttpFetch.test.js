a687edfd8d3f869561a4e6c6038c11332fae6d9c
Kanstantsin Kamkou <me@2ka.by>
2020-03-19 10:22:05 +0100

    codeclimate corrections

--- tests/browser/AdrHttpFetch.test.js
+++ tests/browser/AdrHttpFetch.test.js
@@ -1,14 +1,9 @@
 import AdrHttpFetch from "../../gelfjs/browser/AdrHttpFetch";
 import GfMessage from "../../gelfjs/lib/GfMessage";
+import GelfJs from "../../gelfjs/lib/GelfJs";
 
-/*test('tbd', () => {
-  new AdrHttpFetch.Smart("http://10.89.2.10:12201/gelf");
-  const adapter = new AdrHttpFetch();
-  expect(adapter.send(new GfMessage())).resolves.toEqual(undefined);
-});*/
-
-test('asd', async () => {
-  const adapter = new AdrHttpFetch.Smart("http://10.89.2.10:12201/gelf");
-  //console.log(await adapter.send(new GfMessage()));
-  //expect(adapter.send(new GfMessage())).resolves.toEqual(undefined);
+xtest('asd', async () => {
+  const gelfjs = new GelfJs.Smart(new AdrHttpFetch.Smart("http://xx/gelf")),
+    extra = {tom: 'cat', dogs: {spike: 'bulldog', tyke: 'son of spike'}};
+  await gelfjs.warn('a new message', extra);
 });
